2cc29d449ab4e206bfbe2c5e36fa26dcf14d2e40
Sibaram Sahu <sibaspage@gmail.com>
2024-10-06 20:17:49 +0530

    news and youtube search

--- crew.py
+++ crew.py
@@ -5,18 +5,17 @@ from tasks import yt_research_task,yt_write_task, sp_research_task, sp_write_tas
 
 # Forming the tech-focused crew with some enhanced configurations
 def youtube_search():
-    # yt_crew = Crew(
-    #     agents=[blog_researcher, blog_writer],
-    #     tasks=[yt_research_task, yt_write_task],
-    #     process=Process.sequential,  # Optional: Sequential task execution is default
-    #     memory=True,
-    #     cache=True,
-    #     max_rpm=100,
-    #     share_crew=True
-    # )
-    # result=yt_crew.kickoff(inputs={'topic':'The Naval Podcast - Naval Ravikant with BeerBiceps'})    
-    # print(result)
-    pass
+    yt_crew = Crew(
+        agents=[blog_researcher, blog_writer],
+        tasks=[yt_research_task, yt_write_task],
+        process=Process.sequential,  # Optional: Sequential task execution is default
+        memory=True,
+        cache=True,
+        max_rpm=100,
+        share_crew=True
+    )
+    result=yt_crew.kickoff(inputs={'topic':'The Naval Podcast - Naval Ravikant with BeerBiceps'})    
+    print(result)
 
 
 def news_search():
@@ -29,12 +28,10 @@ def news_search():
     print(result)
 
 def main(search_type = "search"):
-    print(">>>", search_type)
-    news_search()
-    # if search_type == "search":
-    #     youtube_search()    
-    # else:
-    #     news_search()
+    if search_type == "search":
+        youtube_search()
+    else:
+        news_search()
 
 
 if __name__ == "__main__":
